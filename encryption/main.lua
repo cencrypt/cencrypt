098eb7c0944d7c7a2d88609398ce3606baf6e612
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-08 15:44:31 -0400

    Update main.lua

--- encryption/main.lua
+++ encryption/main.lua
@@ -21,6 +21,7 @@ end
 makeModule("AES", "https://raw.githubusercontent.com/idiomic/Lua_AES/master/AES.lua") -- thanks tyler <3
 makeModule("LoadModules", "https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/modules/LoadModules.lua")
 makeModule("CharsBytes", "https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/modules/CharsBytes.lua")
+makeModule("GUID", "https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/modules/GUID.lua")
 
 local AES, LoadModules, CharsBytes -- so there are none of those annoying "undefined variable" warnings in vscode
 
@@ -41,14 +42,35 @@ end
 
 local targetServices = {"Workspace", "Lighting", "ReplicatedFirst", "ReplicatedStorage", "ServerScriptService", "ServerStorage", "StarterGui", "StarterPack", "Teams", "SoundService", "Chat"}
 
+local function getServiceChildren(service)
+    local children = game:GetService(service):GetChildren()
+    local filtered = {}
+    for _, child in pairs(children) do
+        if child ~= modules then
+            table.insert(filtered, child)
+        end
+    end
+    return filtered
+end
+
 warn("Untargeting empty services...")
 
 local goodServices = {}
 
 for _, service in pairs(targetServices) do
-    if #service:GetChildren() > 0 then
+    if #getServiceChildren(service) > 0 then
         table.insert(goodServices, service)
     end
 end
 
 targetServices = goodServices
+
+warn("Grabbing services' children...")
+
+local servicesChildren = {}
+
+for _, service in pairs(targetServices) do
+    servicesChildren[service] = getServiceChildren(service)
+end
+
+warn("Grabbing services' children...")
