89595c29f36173f9a864527be78d737eb863b60a
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-08 14:12:25 -0400

    module loading

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cencrypt
  The power of encryption.
+
+```lua
+loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/encryption/main.lua"))()
+```
--- encryption/main.lua
+++ encryption/main.lua
@@ -1,3 +1,16 @@
+if false then
+    -- use in command bar to execute script
+    loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/encryption/main.lua"))()
+end
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local modules = Instance.new("Folder", ReplicatedStorage)
+modules.Name = "cencrypt modules"
+
 local function makeModule(name, url)
-    
+    local module = Instance.new("ModuleScript", modules)
+    module.Name = name
+    module.Source = game:GetService("HttpService"):GetAsync(url)
+    return module
 end
