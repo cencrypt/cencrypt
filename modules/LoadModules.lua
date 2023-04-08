6f88752f0caeb2781327cd0ad52da4a527c431b4
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-08 14:47:12 -0400

    Update LoadModules.lua

--- modules/LoadModules.lua
+++ modules/LoadModules.lua
@@ -1,7 +1,7 @@
 local modules = script.Parent
 
 return function()
-    for _, module in pairs(modules) do
+    for _, module in pairs(modules:GetChildren()) do
         getfenv(2)[module.Name] = require(module)
     end
 end
