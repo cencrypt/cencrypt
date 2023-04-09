149d9927969ad95e6bbf75a58b662010b9ad2d9d
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-08 20:54:36 -0400

    dev

--- modules/LoadModules.lua
+++ modules/LoadModules.lua
@@ -1,9 +1,18 @@
 local modules = script.Parent
 
-return function()
+return function(bl)
     for _, module in pairs(modules:GetChildren()) do
-        if module ~= getfenv(2).script then
-            getfenv(2)[module.Name] = require(module)
+        local bled = false
+        for _, s in pairs(bl) do
+            if module == s then
+                bled = true
+                break
+            end
+        end
+        if bled == false then
+            if module ~= getfenv(2).script then
+                getfenv(2)[module.Name] = require(module)
+            end
         end
     end
 end
