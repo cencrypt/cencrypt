6d2c3991b48b96ededd647aae3549ddd7fe2fdc0
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-08 20:36:27 -0400

    dev

--- modules/Data.lua
+++
@@ -1,3 +0,0 @@
-local Data = {}
-
-return Data
--- modules/ScriptWrapper.lua
+++ modules/ScriptWrapper.lua
@@ -1,16 +1,23 @@
 local modules = script.Parent
 
+local CharsBytes-- so there are none of those annoying "undefined variable" warnings in vscode
+
 require(modules:WaitForChild("LoadModules"))()
 
 local function wrapScript(scr, quickEncrypt)
     local src = scr.Source
     local srcSecure = quickEncrypt(src)
     local srcBytes = CharsBytes.charsToBytes(srcSecure)
-    return table.concat({""}, "")
+    return table.concat(
+        {
+            'game:GetService("ReplicatedStorage"):WaitForChild("', modules.Name, '"):WaitForChild("LoadModules")\n' ,
+            'return loadstring(ScriptWrapper.unwrapScript("', srcBytes, '")()'
+        }, "")
 end
 
-local function unwrapScript(str)
 
+local function unwrapScript(str)
+    local Data = require(game:WaitForChild("Data"))
 end
 
 return {
