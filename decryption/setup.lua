e0d73482c86f96a5cad4c4bbc6ca2fabe03d0491
cencrypt <130243960+cencrypt@users.noreply.github.com>
2023-04-10 19:34:01 -0400

    Update setup.lua

--- decryption/setup.lua
+++ decryption/setup.lua
@@ -1,4 +1,7 @@
 game:GetService("Players").CharacterAutoLoads = false
 
+local RE = Instance.new("RemoteEvent", game:GetService("ReplicatedStorage"))
+RE.Name = "SENDKEY"
+
 Instance.new("Script", game:GetService("ServerScriptService")).Source = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/decryption/server.lua")
 Instance.new("LocalScript", game:GetService("ReplicatedFirst")).Source = game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/cencrypt/cencrypt/v0.3/decryption/client.lua")
